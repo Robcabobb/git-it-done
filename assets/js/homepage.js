042676ed1239701efefefbf3e11d4eb4546a495f
robcabobb <robcabobb@gmail.com>
2022-01-03 16:25:46 -0500

    added catch function for network errors

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -8,11 +8,19 @@ var getUserRepos = function (user) {
 	var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
 	// make a request to the url
-	fetch(apiUrl).then(function (response) {
-		response.json().then(function (data) {
-			displayRepos(data, user);
+	fetch(apiUrl)
+		.then(function (response) {
+			if (response.ok) {
+				response.json().then(function (data) {
+					displayRepos(data, user);
+				});
+			} else {
+				alert("Error: Github User Not Found");
+			}
+		})
+		.catch(function (erro) {
+			alert("Unable to connect to Github");
 		});
-	});
 };
 
 var formSubmitHandler = function (event) {
@@ -29,6 +37,11 @@ var formSubmitHandler = function (event) {
 };
 
 var displayRepos = function (repos, searchTerm) {
+	// check if api returned any repos
+	if (repos.length === 0) {
+		repoContainerEl.textContent = "No repositories found.";
+		return;
+	}
 	// clear old content
 	repoContainerEl.textContent = "";
 	repoSearchTerm.textContent = searchTerm;
@@ -56,7 +69,9 @@ var displayRepos = function (repos, searchTerm) {
 		// check if current repo has issues or not
 		if (repos[i].open_issues_count > 0) {
 			statusEl.innerHTML =
-			"<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+				"<i class='fas fa-times status-icon icon-danger'></i>" +
+				repos[i].open_issues_count +
+				" issue(s)";
 		} else {
 			statusEl.innerHTML =
 				"<i class='fas fa-check-square status-icon icon-success'></i>";
@@ -64,7 +79,6 @@ var displayRepos = function (repos, searchTerm) {
 
 		// append to container
 		repoEl.appendChild(statusEl);
-
 
 		// append container to the dom
 		repoContainerEl.appendChild(repoEl);
